be1a5bf937423d660f1d9d5027a2d405a858d7b1
qq5621 <qq5621@noreply.gitcode.com>
2026-03-04 11:16:51 +0800

    feat: 前端渲染gojs組件案例

---
+++ business-behavior-monitor-admin/src/test/java/cn/xpeanut/monitor/test/ApiTest.java
@@ -0,0 +1,19 @@
+package cn.xpeanut.monitor.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ApiTest {
+
+    @Test
+    private void test() {
+        log.info("test");
+    }
+
+}
